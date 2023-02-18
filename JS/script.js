e4fab8427044e20282b338bb603fa37c02be865f
Sakib Hossain <sakib199988@gmail.com>
2023-02-18 22:29:32 +0600

    pentagon and elipse calculation added

--- JS/script.js
+++ JS/script.js
@@ -34,6 +34,26 @@ document.getElementById("triangle-btn").addEventListener("click", function () {
       newElementAdd(area, "Rhombus");
   
   });
+  //pentagon calculation
+  document.getElementById('pentagon-btn').addEventListener('click',function(){
+    const firstValue = getValueFromInput('pentagon-first');
+    const secondValue = getValueFromInput('pentagon-second');
+    const area = 0.5 * firstValue * secondValue;
+    setValueFormula('pen-a','pen-b',firstValue,secondValue);
+    newElementAdd(area, "Pentagon");
+
+});
+
+//ellipse calculation
+document.getElementById('ellipse-btn').addEventListener('click',function(){
+    const firstValue = getValueFromInput('ellipse-first');
+    const secondValue = getValueFromInput('ellipse-second');
+    const area = 3.1416 * firstValue * secondValue;
+    setValueFormula('ell-a','ell-b',firstValue,secondValue);
+    newElementAdd(area, "Ellipse");
+
+});
+
   
   //blogs button 
   
